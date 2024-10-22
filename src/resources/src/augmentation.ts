3ad976bc1df152354ed049dbe750d4e84ee2bec1
jackbrett <musicsnobj@gmail.com>
2024-10-22 10:59:41 -0500

    connect Craft plugin to CKEditor plugin, add dll:watch script

--- src/resources/src/augmentation.ts
+++ src/resources/src/augmentation.ts
@@ -2,6 +2,6 @@ import type { ThesaurusLex } from './index.js';
 
 declare module '@ckeditor/ckeditor5-core' {
 	interface PluginsMap {
-		[ ThesaurusLex.pluginName ]: ThesaurusLex;
+		[ThesaurusLex.pluginName]: ThesaurusLex;
 	}
 }
