4ecd0b9145d4949450ffa8687ac307683e4aba3a
jackbrett <musicsnobj@gmail.com>
2024-10-23 15:42:48 -0500

    sense sequence block helper

--- src/resources/src/ui/singlemeaningview.ts
+++ src/resources/src/ui/singlemeaningview.ts
@@ -11,13 +11,36 @@ export default class SingleMeaningView extends View {
 	constructor(locale: Locale, result: DictionaryTypes.DictionaryResult) {
 		super(locale);
 
-		const { hwi, shortdef: shortDefinitions } = result;
+		const { def, hwi, shortdef: shortDefinitions } = result;
+		const { sseq: senseSequences } = def[0];
 		const { prs: pronunciations } = hwi;
 
 		const pronunciationsContainer = this.createPronunciationsBlock(
 			pronunciations,
 			locale,
 		);
+
+		const senseSequenceBlockCollection = this.createCollection();
+
+		if (senseSequences.length) {
+			senseSequences.forEach((senseSequence, index) => {
+				console.log(`sense sequence: ${index}`);
+				const senseSequenceBlock = this.createSenseSequenceBlock(
+					senseSequence,
+					locale,
+				);
+				senseSequenceBlockCollection.add(senseSequenceBlock);
+			});
+		}
+
+		const sequencesContainer = new View(locale);
+		sequencesContainer.setTemplate({
+			tag: 'div',
+			attributes: {
+				class: ['ck'],
+			},
+			children: senseSequenceBlockCollection,
+		});
 
 		const definitionsContainer = this.createShortDefinitionsBlock(
 			shortDefinitions,
@@ -29,42 +52,12 @@ export default class SingleMeaningView extends View {
 			attributes: {
 				class: ['ck', 'ck-definitions'],
 			},
-			children: [pronunciationsContainer, definitionsContainer],
+			children: [
+				pronunciationsContainer,
+				// definitionsContainer,
+				sequencesContainer,
+			],
 		});
-	}
-
-	// @todo figure out why <audio> blocks won't render in CKEditor
-	createAudioBlock(
-		pronunciation: DictionaryTypes.Pronunciation,
-		locale: Locale,
-	): View | undefined {
-		const { mw, sound } = pronunciation;
-		if (!sound) {
-			return undefined;
-		}
-		const url = getAudioUrlForPronunciation(sound);
-		if (!url) {
-			return undefined;
-		}
-		// const sourceBlock = new View(locale);
-		// sourceBlock.setTemplate({
-		// 	tag: 'source',
-		// 	attributes: {
-		// 		src: url,
-		// 		type: 'audio/wav',
-		// 	},
-		// });
-		const audioBlock = new View(locale);
-		audioBlock.setTemplate({
-			tag: 'audio',
-			attributes: {
-				controls: true,
-				src: url,
-				type: 'audio/wav',
-			},
-			// children: [sourceBlock]
-		});
-		return audioBlock;
 	}
 
 	createPronunciationsBlock(
@@ -101,7 +94,7 @@ export default class SingleMeaningView extends View {
 					} = pronunciation;
 
 					if (preText) {
-            // append preText value to pronunciation, if defined
+						// append preText value to pronunciation, if defined
 						const preTextBlock = new View(locale);
 						preTextBlock.setTemplate({
 							tag: 'p',
@@ -112,10 +105,6 @@ export default class SingleMeaningView extends View {
 						});
 						pronunciationBlocks.add(preTextBlock);
 					}
-					const audioBlock = this.createAudioBlock(
-						pronunciation,
-						locale,
-					);
 
 					const pronunciationBlock = new View(locale);
 					pronunciationBlock.setTemplate({
@@ -128,7 +117,7 @@ export default class SingleMeaningView extends View {
 					pronunciationBlocks.add(pronunciationBlock);
 
 					if (postText) {
-            // append postText value to pronunciation, if defined
+						// append postText value to pronunciation, if defined
 						const postTextBlock = new View(locale);
 						postTextBlock.setTemplate({
 							tag: 'p',
@@ -176,5 +165,85 @@ export default class SingleMeaningView extends View {
 			children: shortDefinitionBlocks,
 		});
 		return shortDefinitionsContainer;
+	}
+
+	createDefiningTextBlock(
+		definingText: DictionaryTypes.DefiningText,
+		locale: Locale,
+	): View {
+		console.log(`createDefiningTextBlock input: `, definingText);
+		const definition = definingText[1];
+		const definitionBlock = new View(locale);
+		definitionBlock.setTemplate({
+			tag: 'p',
+			attributes: {
+				class: ['ck', 'ck-definition'],
+			},
+			children: [definition],
+		});
+		return definitionBlock;
+	}
+
+	createSenseBlock(sense: DictionaryTypes.Sense, locale: Locale): View {
+		// console.log(`createSenseBlock input: `, sense);
+		const { sn: senseNumber, dt, et } = sense[1];
+		console.log(`senseNumber: ${senseNumber}`);
+
+		const senseCollection = this.createCollection();
+
+		if (senseNumber) {
+			const snBlock = new View(locale);
+			snBlock.setTemplate({
+				tag: 'span',
+				attributes: {
+					class: ['ck', 'ck-sense-number'],
+				},
+				children: [senseNumber],
+			});
+			senseCollection.add(snBlock);
+		}
+
+		if (dt.length && dt[0].length && dt[0][0] === 'text') {
+			const definingTextBlock = this.createDefiningTextBlock(
+				dt[0],
+				locale,
+			);
+			senseCollection.add(definingTextBlock);
+		}
+
+		const dtRowBlock = new View(locale);
+		dtRowBlock.setTemplate({
+			tag: 'p',
+			attributes: {
+				class: ['ck', 'ck-dt-row'],
+			},
+			children: senseCollection,
+		});
+
+		return dtRowBlock;
+	}
+
+	createSenseSequenceBlock(
+		senseSequence: DictionaryTypes.SenseSequence,
+		locale: Locale,
+	): View {
+		// console.log(`createSenseSequenceBlock input: `, senseSequence);
+		const sceneSequenceCollection = this.createCollection();
+
+		senseSequence.forEach((sense, index) => {
+			// console.log(`sense: ${index}`);
+			const senseBlock = this.createSenseBlock(sense, locale);
+			sceneSequenceCollection.add(senseBlock);
+		});
+
+		const senseSequenceBlock = new View(locale);
+		senseSequenceBlock.setTemplate({
+			tag: 'div',
+			attributes: {
+				class: ['ck', 'ck-sense-sequence'],
+			},
+			children: sceneSequenceCollection,
+		});
+		return senseSequenceBlock;
 	}
 }
